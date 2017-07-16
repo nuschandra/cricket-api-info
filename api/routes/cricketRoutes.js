3094732f8528d9098628f63bece849c16f8bd64f
Viswanathan <cviswana@visa.com>
2017-07-17 01:40:07 +0800

    adding list matches by type

--- api/routes/cricketRoutes.js
+++ api/routes/cricketRoutes.js
@@ -3,6 +3,15 @@
 module.exports=function(app){
 	var cricketController=require('../controllers/cricketController');
 
-	app.route('/currentMatches')
-		.get(cricketController.listCurrentMatches);
+//	app.route('/currentMatches?')
+//		.get(cricketController.listCurrentMatches);
+
+	app.get('/currentMatches?',function(req,res,next){
+		if(req.query.matchType!=null){
+			cricketController.listCurrentMatchesByType(req.query.matchType);
+		}
+		else{
+			cricketController.listCurrentMatches();
+		}
+	});
 };
