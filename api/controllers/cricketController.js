7a0edd8f10685ac5084115736eeb9c825c0b632c
System Administrator <cviswana@visa.com>
2017-09-05 10:13:02 +0800

    returning empty instead of error for matches with no scores

--- api/controllers/cricketController.js
+++ api/controllers/cricketController.js
@@ -56,46 +56,53 @@ exports.currentScore=function(req,res){
 				console.log('Error');
 			}
 			else{
-				var stringify=JSON.stringify(results)
-				var results=JSON.parse(stringify)
-				Scores[matchId]=req.query.id;
-				Scores[teamScores][teamOne]=results[0]["TEAM_ONE_SCORE"];
-				Scores[teamScores][teamTwo]=results[0]["TEAM_TWO_SCORE"];
-				var batsmenOne={
-					batsmanName:results[0]["BATSMAN_ONE_NAME"],
-					batsmanScore:results[0]["BATSMAN_ONE_SCORE"],
-					batsmanBoundaries:results[0]["BATSMAN_ONE_BOUNDARIES"],
-					batsmanStrikeRate:results[0]["BATSMAN_ONE_SR"]
-				};
-				var batsmenTwo={
-					batsmanName:results[0]["BATSMAN_TWO_NAME"],
-					batsmanScore:results[0]["BATSMAN_TWO_SCORE"],
-					batsmanBoundaries:results[0]["BATSMAN_TWO_BOUNDARIES"],
-					batsmanStrikeRate:results[0]["BATSMAN_TWO_SR"]
-				};
-				var bowlerOne={
-					bowlerName:results[0]["BOWLER_ONE_NAME"],
-					bowlerOvers:results[0]["BOWLER_ONE_OVERS"],
-					bowlerMaidens:results[0]["BOWLER_ONE_MAIDENS"],
-					bowlerRuns:results[0]["BOWLER_ONE_RUNS"],
-					bowlerWickets:results[0]["BOWLER_ONE_WICKETS"],
-					bowlerEconomy:results[0]["BOWLER_ONE_ECO"]
-				};
-				var bowlerTwo={
-					bowlerName:results[0]["BOWLER_TWO_NAME"],
-					bowlerOvers:results[0]["BOWLER_TWO_OVERS"],
-					bowlerMaidens:results[0]["BOWLER_TWO_MAIDENS"],
-					bowlerRuns:results[0]["BOWLER_TWO_RUNS"],
-					bowlerWickets:results[0]["BOWLER_TWO_WICKETS"],
-					bowlerEconomy:results[0]["BOWLER_TWO_ECO"]
-				};
-				Scores[batsmen].push(batsmenOne);
-				Scores[batsmen].push(batsmenTwo);
-				Scores[bowlers].push(bowlerOne);
-				Scores[bowlers].push(bowlerTwo);
-				ScoresArray.push(Scores)
-				var scoreJson=JSON.stringify(ScoresArray);
-				res.send(scoreJson);
+				if(results.length!=0){
+					var stringify=JSON.stringify(results)
+					var results=JSON.parse(stringify)
+					Scores[matchId]=req.query.id;
+					Scores[teamScores][teamOne]=results[0]["TEAM_ONE_SCORE"];
+					Scores[teamScores][teamTwo]=results[0]["TEAM_TWO_SCORE"];
+					var batsmenOne={
+						batsmanName:results[0]["BATSMAN_ONE_NAME"],
+						batsmanScore:results[0]["BATSMAN_ONE_SCORE"],
+						batsmanBoundaries:results[0]["BATSMAN_ONE_BOUNDARIES"],
+						batsmanStrikeRate:results[0]["BATSMAN_ONE_SR"]
+					};
+					var batsmenTwo={
+						batsmanName:results[0]["BATSMAN_TWO_NAME"],
+						batsmanScore:results[0]["BATSMAN_TWO_SCORE"],
+						batsmanBoundaries:results[0]["BATSMAN_TWO_BOUNDARIES"],
+						batsmanStrikeRate:results[0]["BATSMAN_TWO_SR"]
+					};
+					var bowlerOne={
+						bowlerName:results[0]["BOWLER_ONE_NAME"],
+						bowlerOvers:results[0]["BOWLER_ONE_OVERS"],
+						bowlerMaidens:results[0]["BOWLER_ONE_MAIDENS"],
+						bowlerRuns:results[0]["BOWLER_ONE_RUNS"],
+						bowlerWickets:results[0]["BOWLER_ONE_WICKETS"],
+						bowlerEconomy:results[0]["BOWLER_ONE_ECO"]
+					};
+					var bowlerTwo={
+						bowlerName:results[0]["BOWLER_TWO_NAME"],
+						bowlerOvers:results[0]["BOWLER_TWO_OVERS"],
+						bowlerMaidens:results[0]["BOWLER_TWO_MAIDENS"],
+						bowlerRuns:results[0]["BOWLER_TWO_RUNS"],
+						bowlerWickets:results[0]["BOWLER_TWO_WICKETS"],
+						bowlerEconomy:results[0]["BOWLER_TWO_ECO"]
+					};
+					Scores[batsmen].push(batsmenOne);
+					Scores[batsmen].push(batsmenTwo);
+					Scores[bowlers].push(bowlerOne);
+					Scores[bowlers].push(bowlerTwo);
+					ScoresArray.push(Scores)
+					var scoreJson=JSON.stringify(ScoresArray);
+					res.send(scoreJson);
+
+				}
+				else{
+					var scoreJson=JSON.stringify(ScoresArray);
+					res.send(scoreJson); 
+				}
 			}
 		});
 	}
